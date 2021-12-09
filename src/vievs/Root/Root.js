aac76d0f2f732315ed037fd3e1c81e075df387e8
Kamil Andziak <kamil.andziakk97@gmail.com>
2021-12-09 20:07:56 +0100

    Start Projekt

---
+++ src/vievs/Root/Root.js
@@ -0,0 +1,14 @@
+// import styled from 'styled-components';
+import React from 'react';
+
+const Root = function () {
+  return (
+    <div>
+      {' '}
+      Hello
+      <div> siema </div>
+    </div>
+  );
+};
+
+export default Root;
